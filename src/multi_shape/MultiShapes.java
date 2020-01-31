7e91164b0ab1ca85e2355998b8c6a4db4b3b378c
Abdelrahman Mashael <abdomashael@yahoo.com>
2020-01-31 18:22:49 +0200

    add repo link

--- src/multi_shape/MultiShapes.java
+++ src/multi_shape/MultiShapes.java
@@ -9,14 +9,9 @@ import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.util.Vector;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * @author mashael
+ * GitHub Repo: https://github.com/abdomashael/MultiShape
  */
 public class MultiShapes extends Applet {
 
